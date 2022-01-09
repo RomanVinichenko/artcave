6aa61c39dfcdd8427917fa04f13a92fb53a1cfb9
Roman Vinichenko <chicagezzz@gmail.com>
2022-01-09 15:38:09 +0200

    added header and footer

--- gulpfile.js
+++ gulpfile.js
@@ -21,7 +21,7 @@ const browsersync = () => {
 const watching = () => {
     watch(['app/*.html']).on('change', browserSync.reload);
     watch(['app/scss/**/*.scss'], styles);
-    watch(['app/js/index.js', '!app/js/index.min.js', '!app/js/index.min.js.map'], scripts);
+    watch(['!app/js/main.js', '!app/js/faq.js', '!app/js/terms.js'], scripts);
 };
 
 const styles = () => {
@@ -41,11 +41,7 @@ const styles = () => {
 };
 
 const scripts = () => {
-    return src(['app/js/index.js'])
-        .pipe(sourcemaps.init())
-        .pipe(concat('index.min.js'))
-        .pipe(uglify())
-        .pipe(sourcemaps.write('.'))
+    return src(['app/js/main.js', 'app/js/faq.js', 'app/js/terms.js'])
         .pipe(dest('app/js'))
         .pipe(browserSync.stream());
 };
@@ -65,8 +61,7 @@ const build = () => {
         [
             'app/css/style.min.css',
             'app/css/style.min.css.map',
-            'app/js/index.min.js',
-            'app/js/index.min.js.map',
+            'app/js/**/*.js',
             'app/fonts/**/*',
             'app/images/**/*',
         ],
