16edf95200387725d1c98ae0df305a20f87a8af5
RomanVinichenko <chicagezzz@gmail.com>
2022-01-13 16:50:25 +0200

    Updates

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(function () {
+    $('.articles__list').slick({
+        loop: true,
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        // autoplay: true,
+        autoplaySpeed: 2000,
+        dots: false,
+        arrows: false,
+    });
+});
