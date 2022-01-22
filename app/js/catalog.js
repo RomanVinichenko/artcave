c662aa18c03aa9aac1a51303641dfeba773ea0b8
RomanVinichenko <chicagezzz@gmail.com>
2022-01-22 15:52:27 +0200

    fix dropdowm + contact from

--- app/js/catalog.js
+++
@@ -1,8 +0,0 @@
-document.querySelectorAll('.catalog__button--choose').forEach(el => {
-    el.addEventListener('click', function()  {
-        this.classList.toggle('catalog__button--active')
-    } )
-})
-
-
-
