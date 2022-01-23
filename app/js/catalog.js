6804bf8d9932fe4b45460c332c99e6be40b0ba95
RomanVinichenko <chicagezzz@gmail.com>
2022-01-23 18:48:51 +0200

    start adaptive

---
+++ app/js/catalog.js
@@ -0,0 +1,12 @@
+$(function () {
+    $('.major__items').slick({
+        loop: true,
+        infinite: true,
+        slidesToShow: 6,
+        slidesToScroll: 6,
+        // autoplay: true,
+        autoplaySpeed: 2000,
+        dots: false,
+        arrows: false,
+    });
+});
--- app/js/main.js
+++ app/js/main.js
@@ -10,3 +10,4 @@ $(function () {
         arrows: true,
     });
 });
+
