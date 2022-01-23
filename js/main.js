b9e84c80699a63d8055507fc0675938ab8e1f0f0
RomanVinichenko <chicagezzz@gmail.com>
2022-01-24 00:51:08 +0200

    Updates

--- js/main.js
+++ js/main.js
@@ -4,10 +4,30 @@ $(function () {
         infinite: true,
         slidesToShow: 4,
         slidesToScroll: 1,
-        autoplay: true,
+        // autoplay: true,
         autoplaySpeed: 2000,
         dots: false,
         arrows: true,
+        responsive: [
+            {
+                breakpoint: 1550,
+                settings: {
+                    slidesToShow: 3,
+                },
+            },
+            {
+                breakpoint: 930,
+                settings: {
+                    slidesToShow: 2,
+                },
+            },
+            {
+                breakpoint: 750,
+                settings: {
+                    slidesToShow: 1,
+                },
+            }
+        ]
     });
 });
 
